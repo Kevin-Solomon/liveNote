f61da880c7bbaa5b3c0d3aaa638669fb3281b511
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-03-28 01:47:56 +0530

    refactor : imported packages

--- src/context/notes/noteContext.js
+++ src/context/notes/noteContext.js
@@ -1,11 +1,14 @@
 import { createContext, useContext, useReducer, useEffect } from 'react';
 import { noteReducer, noteInitialState } from '../../reducer/noteReducer';
+import axios from 'axios';
+import { useAuth } from '../auth/authContext';
 const NoteContext = createContext({ notes: [] });
 const NoteProvider = ({ children }) => {
+  const { user } = useAuth();
   const [noteState, noteDispatch] = useReducer(noteReducer, noteInitialState);
   useEffect(() => {
     const getNotes = () => {
-      const response = axios.get('/api/notes', { auth });
+      const response = axios.get('/api/notes', { authorization: user.token });
     };
     getNotes();
   }, []);

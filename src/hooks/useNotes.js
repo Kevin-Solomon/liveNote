ac75cc9f55cc49a596740567e8236ce9bc039d0c
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-03-27 14:16:50 +0530

    feat : add sidebar

--- src/hooks/useNotes.js
+++ src/hooks/useNotes.js
@@ -1,4 +1,8 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useAuth } from './../context/auth/authContext';
 export function useNotes() {
+  const { user } = useAuth();
   const [notes, setNotes] = useState([]);
   useEffect(() => {
     const getProducts = async () => {
@@ -11,7 +15,7 @@ export function useNotes() {
         console.log(response.data.notes);
         setNotes([...response.data.notes]);
       } catch (err) {
-        console.log(err.response);
+        console.log(err);
       }
     };
     getProducts();

9032a275eb0df0ac834199da5a48e7989a23384d
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-03-28 01:39:14 +0530

    feat : initial get request for notes

--- src/context/notes/noteContext.js
+++ src/context/notes/noteContext.js
@@ -3,6 +3,11 @@ import { noteReducer, noteInitialState } from '../../reducer/noteReducer';
 const NoteContext = createContext({ notes: [] });
 const NoteProvider = ({ children }) => {
   const [noteState, noteDispatch] = useReducer(noteReducer, noteInitialState);
+  useEffect(() => {
+    const getNotes = () => {
+      const response = axios.get('/api/notes', { auth });
+    };
+  }, []);
   return (
     <NoteContext.Provider value={{ noteState, noteDispatch }}>
       {children}

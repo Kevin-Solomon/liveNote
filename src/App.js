fa99afae88aead36a7b228aa636229afaaf6a510
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-04-06 18:03:50 +0530

    feat : made few routes protected

--- src/App.js
+++ src/App.js
@@ -9,19 +9,62 @@ import NoteEditor from './pages/NoteEditor/NoteEditor';
 import Archive from './pages/Archive/Archive';
 import Trash from './pages/Trash/Trash';
 import Label from './pages/Label/Label';
+import AuthRequired from './components/AuthRequired/AuthRequired';
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/archive" element={<Archive />} />
+      <Route
+        path="/home"
+        element={
+          <AuthRequired>
+            <Home />
+          </AuthRequired>
+        }
+      />
+      <Route
+        path="/archive"
+        element={
+          <AuthRequired>
+            <Archive />
+          </AuthRequired>
+        }
+      />
       <Route path="/login" element={<Login />} />
-      <Route path="/trash" element={<Trash />} />
+      <Route
+        path="/trash"
+        element={
+          <AuthRequired>
+            <Trash />
+          </AuthRequired>
+        }
+      />
       <Route path="/mockman" element={<Mockman />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/singlenote" element={<NoteEditor />} />
-      <Route path="/singlenote/:singlenoteId" element={<NoteEditor />} />
-      <Route path="/label" element={<Label />} />
+      <Route
+        path="/singlenote"
+        element={
+          <AuthRequired>
+            <NoteEditor />
+          </AuthRequired>
+        }
+      />
+      <Route
+        path="/singlenote/:singlenoteId"
+        element={
+          <AuthRequired>
+            <NoteEditor />
+          </AuthRequired>
+        }
+      />
+      <Route
+        path="/label"
+        element={
+          <AuthRequired>
+            <Label />
+          </AuthRequired>
+        }
+      />
     </Routes>
   );
 }
